5fccfaab0d9f2a386cf99eeba92c43a2110f3eab
Vinyurchin <91989971+Vinyurchin@users.noreply.github.com>
2021-10-29 00:06:42 -0500

    Add files via upload

---
+++ Función_4.m
@@ -0,0 +1,17 @@
+% De secuencia de comandos de octava
+% Título: Función 4
+% Description: Script para recordar las funciones reales
+% Autor: Cruz Garduño Diego
+% Fecha: 28/10/2021
+% Notas: http: //octaveintro.readthedocs.io/en/latest/index.html
+
+claro
+
+% dominio de la función
+x = - 5 : 1 : - 1 ;
+
+% Regla de correspondencia (función)
+fx = ( x . ^ 2 ) + ( 6 * x )
+
+% plotear función
+trama ( x , fx )
